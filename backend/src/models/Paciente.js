effd7dec74a7af6565f7e344c3d7fb615754160e
Danna <danna.mendoza@tecsup.edu.pe>
2026-06-08 00:46:14 -0500

    mejorando paciente

--- backend/src/models/Paciente.js
+++ backend/src/models/Paciente.js
@@ -1,11 +1,43 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
-const Paciente = sequelize.define("Paciente", {
-  dni: { type: DataTypes.STRING, unique: true },
-  nombres: DataTypes.STRING,
-  telefono: DataTypes.STRING,
-}, {
-  tableName: "pacientes",   
-  timestamps: false
-});
+
+const Paciente = sequelize.define(
+  "Paciente",
+  {
+    dni: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+
+    nombres: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    apellidos: {
+      type: DataTypes.STRING,
+    },
+
+    telefono: {
+      type: DataTypes.STRING,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+    },
+
+    fecha_nacimiento: {
+      type: DataTypes.DATEONLY,
+    },
+
+    sexo: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    tableName: "pacientes",
+    timestamps: false,
+  }
+);
+
 export default Paciente;
